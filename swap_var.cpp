4e4bbf2c9e2c5a00933398d7466a28e592bef3b8
akbuluth15 <50354100+akbuluth15@users.noreply.github.com>
2020-05-19 14:51:41 +0300

    Create swap_var.cpp

---
+++ swap_var.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 4;
+  int b = 5;
+  
+  a = a + b;  // a becomes 9
+  b = a - b;  // b becomes 4
+  a = a - b;  // a becomes 5
+  
+  cout << "After Swapping: x =" << x << ", y=" << y;
+
+}
